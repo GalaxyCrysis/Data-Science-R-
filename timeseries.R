77a6849b1bac5932ee949945873387b1ab2f9a81
Dominik Niedermaier <galaxycrysis11@gmail.com>
2016-09-07 16:57:36 +0200

    Update timeseries.R

--- timeseries.R
+++ timeseries.R
@@ -1,6 +1,8 @@
 library(DBI)
 library(RMySQL)
 library(ggplot2)
+library(xts)
+library(TTR)
 
 #connect to database
 connection <- dbConnect(RMySQL::MySQL(),dbname="stocks", user ="root",password="password")
@@ -18,7 +20,19 @@ apple$date <- as.Date(as.character(apple$date),format="%Y%m%d")
 #plot the data
 ggplot(data=apple, aes(x=date,y=closep,group=1))+ 
   geom_line(colour="red",linetype="dashed",size=1.7)
+  
+#now i wanna compare openp and closep so we plot both at the same time
+components <-  decompose(apple)
+ts.plot(components)
 
+#now i wanna plot the average with TTR MA function
+moving_average = SMA(apple$closep,n=15)
+apple <- rbind(applee, moving_average)
+ggplot(data=apple, aes(date))
++ geom_line(aes(y=closep),colour="red",linetype="dashed",size=1.7)
++ geom_line(aes(y=moving_average), colour="blue", linetype="dashed", size=1.7)
+
+#statistics
 #print statistics of closed price of the stock
 print(summary(apple$closep))
 
